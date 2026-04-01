db7ff6c85fef29be7478d61882963672f1a9ce22
baaaa <zevzeklu097@gmail.com>
2026-04-01 21:15:53 +0300

    Update and rename zimbirt.ps1 to payload.sct

--- zimbirt.ps1
+++
@@ -1,5 +0,0 @@
-# payload.ps1
-# This will create a file on the user's desktop with the parameters used
-$desktop = [Environment]::GetFolderPath("Desktop")
-$outputFile = Join-Path $desktop "payload_test.txt"
-"type=$type`nfile=$file`nfile_path=$file_path`nshow_screen=$show_screen" | Out-File $outputFile
